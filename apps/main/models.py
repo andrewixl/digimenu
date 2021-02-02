8e8905422ca2530f41ab52efbb0f89950d93f714
Andrew W. Burger <andrew.burger@awbtech.org>
2021-02-01 20:19:30 -0800

    Test Run #1

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    active = models.BooleanField(verbose_name='Active?', default=True)
+    menuName = models.CharField(verbose_name='Menu Name', max_length = 50)
+    menuPDF = models.FileField(verbose_name='Menu PDF', upload_to="media/")
+    slug = models.SlugField(verbose_name='slug', max_length = 50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.menuName
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Menu
+from django.http import FileResponse
 
 # Create your views here.
+def index(request):
+    return render( request, 'main/Dennys-Menu.html')
+
+def menu(request, slug):
+    menu = Menu.objects.get(slug=slug)
+    print (menu.menuPDF)
+    return FileResponse(open("media/" + str(menu.menuPDF), 'rb'), content_type='application/pdf')
